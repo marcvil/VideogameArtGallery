587cb6ccfe0f67fe7e0afbdbae5086a148692f97
marcvil <marcvilabascara@gmail.com>
2020-09-15 11:53:35 +0200

    Change Sql to SQL Server

--- DAL/Migrations/20200914094144_Initial.Designer.cs
+++ DAL/Migrations/20200914094144_Initial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20200910154351_Initial")]
+    [Migration("20200914094144_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
